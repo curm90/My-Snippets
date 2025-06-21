4357fb39750c64417dc6a51cba0d6917009f0cd5
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-06-21 10:08:29 +0100

    add theme provider and toggle button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Navigation from '@/components/Navigation/Navigation';
+import { ThemeProvider } from '@/components/ThemeProvider';
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,10 +25,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Navigation />
-				{children}
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navigation />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
