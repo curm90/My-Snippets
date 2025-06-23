c0687674639ec4bff1d56fb9db0b94a436149b90
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-06-23 10:49:41 +0100

    feat: enhance CreateSnippetForm with language selection and code editor

--- src/components/CreateSnippetForm/CreateSnippetForm.tsx
+++ src/components/CreateSnippetForm/CreateSnippetForm.tsx
@@ -2,14 +2,20 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import CodeMirror from '@uiw/react-codemirror';
+import { nord } from '@uiw/codemirror-theme-nord';
+import { langs, loadLanguage, langNames } from '@uiw/codemirror-extensions-langs';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import LanguageSelect from '@/components/LanguageSelect/LanguageSelect';
 
 const formSchema = z.object({
 	title: z.string().min(2).max(50),
+	language: z.enum(Object.keys(langs) as [keyof typeof langs, ...Array<keyof typeof langs>]), // Assert as a tuple
+	snippet: z.string().min(1, 'Snippet is required'),
 });
 
 export default function CreateSnippetForm() {
@@ -17,14 +23,16 @@ export default function CreateSnippetForm() {
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			title: '',
+			language: undefined,
+			snippet: '',
 		},
 	});
 
 	function onSubmit(values: z.infer<typeof formSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
 		console.log(values);
 	}
+
+	console.log({ langs, langNames });
 
 	return (
 		<Form {...form}>
@@ -43,7 +51,40 @@ export default function CreateSnippetForm() {
 							</FormItem>
 						)}
 					/>
+					<FormField
+						control={form.control}
+						name='language'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='font-semibold'>Language</FormLabel>
+								<FormControl>
+									<LanguageSelect {...field} options={langNames} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 				</div>
+				<FormField
+					control={form.control}
+					name='snippet'
+					render={({ field }) => {
+						const selectedLang = form.getValues('language');
+						const languageExtension = loadLanguage(selectedLang) || [];
+
+						return (
+							<FormItem>
+								<FormLabel className='font-semibold'>Snippet</FormLabel>
+								<FormControl>
+									<div className='border border-input bg-transparent rounded-md overflow-hidden py-1 min-h-24'>
+										<CodeMirror extensions={[languageExtension]} theme={nord} {...field} />
+									</div>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						);
+					}}
+				/>
 				<div className='flex items-center justify-end space-x-2'>
 					<Button type='button' variant='outline' className='cursor-pointer' onClick={() => form.reset()}>
 						Cancel
