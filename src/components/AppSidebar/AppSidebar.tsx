ff0d79f769ee96cabffb3166ebc4725a71eea895
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-06-23 06:53:47 +0100

    feat: implement AppSidebar component with navigation to create snippet

---
+++ src/components/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+const items = [
+	{
+		id: 1,
+		title: 'Create Snippet',
+		url: '#',
+	},
+];
+
+export default function AppSidebar() {
+	return (
+		<Sidebar className='h-[calc(100vh-var(--navbar-height))] border-r-4 border-r-secondary'>
+			<SidebarContent className='px-4 py-6'>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton className='bg-action text-center hover:bg-chart-4/85 cursor-pointer'>
+							<Link href='/create-snippet' className='text-md font-bold text-center w-full'>
+								Create Snippet
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarContent>
+		</Sidebar>
+	);
+}
