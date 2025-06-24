43fe6f8eb58f05c093d622fd08c13e7c3231f262
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-06-24 10:23:44 +0100

    feat: add snippet page to display individual snippet details

---
+++ src/app/snippet/[snippetId]/page.tsx
@@ -0,0 +1,22 @@
+import prisma from '@/lib/prisma';
+
+export default async function Page({ params }: { params: { snippetId: string } }) {
+	const { snippetId } = await params;
+	const snippet = await prisma.snippet.findUnique({
+		where: { id: snippetId },
+	});
+
+	if (!snippet) {
+		return <div className='text-red-500'>Snippet not found</div>;
+	}
+
+	const { id } = snippet || {};
+
+	console.log({ snippetId, snippet });
+
+	return (
+		<section className='py-4 px-6'>
+			<div>{id}</div>
+		</section>
+	);
+}
