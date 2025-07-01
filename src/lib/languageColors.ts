eaa867c72df63c645cadf34efe885d570f816b8d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-07-01 10:26:36 +0100

    feat: add language color mapping and utility functions for snippet language badges

---
+++ src/lib/languageColors.ts
@@ -0,0 +1,681 @@
+/**
+ * Language color mapping for snippet language badges
+ * Auto-generated from CodeMirror supported languages (126 total)
+ * Each language has a primary color and background/border variants
+ */
+
+export type LanguageColorConfig = {
+	bg: string;
+	border: string;
+	text: string;
+};
+
+export const languageColors: Record<string, LanguageColorConfig> = {
+	angular: {
+		bg: 'bg-cyan-500/20',
+		border: 'border-cyan-500/50',
+		text: 'text-cyan-600 dark:text-cyan-400',
+	},
+	apl: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	asciiarmor: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	asterisk: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	brainfuck: {
+		bg: 'bg-pink-500/20',
+		border: 'border-pink-500/50',
+		text: 'text-pink-600 dark:text-pink-400',
+	},
+	c: {
+		bg: 'bg-gray-600/20',
+		border: 'border-gray-600/50',
+		text: 'text-gray-700 dark:text-gray-300',
+	},
+	ceylon: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	clojure: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	cmake: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	cobol: {
+		bg: 'bg-pink-500/20',
+		border: 'border-pink-500/50',
+		text: 'text-pink-600 dark:text-pink-400',
+	},
+	coffeescript: {
+		bg: 'bg-amber-600/20',
+		border: 'border-amber-600/50',
+		text: 'text-amber-700 dark:text-amber-300',
+	},
+	commonlisp: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	cpp: {
+		bg: 'bg-gray-600/20',
+		border: 'border-gray-600/50',
+		text: 'text-gray-700 dark:text-gray-300',
+	},
+	crystal: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	csharp: {
+		bg: 'bg-purple-500/20',
+		border: 'border-purple-500/50',
+		text: 'text-purple-600 dark:text-purple-400',
+	},
+	css: {
+		bg: 'bg-blue-600/20',
+		border: 'border-blue-600/50',
+		text: 'text-blue-700 dark:text-blue-300',
+	},
+	cypher: {
+		bg: 'bg-blue-800/20',
+		border: 'border-blue-800/50',
+		text: 'text-blue-900 dark:text-blue-100',
+	},
+	d: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	dart: {
+		bg: 'bg-green-500/20',
+		border: 'border-green-500/50',
+		text: 'text-green-600 dark:text-green-400',
+	},
+	diff: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	dockerfile: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	dtd: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	dylan: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	ebnf: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	ecl: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	eiffel: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	elm: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	erlang: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	factor: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	fcl: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	forth: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	fortran: {
+		bg: 'bg-indigo-500/20',
+		border: 'border-indigo-500/50',
+		text: 'text-indigo-600 dark:text-indigo-400',
+	},
+	gas: {
+		bg: 'bg-red-700/20',
+		border: 'border-red-700/50',
+		text: 'text-red-800 dark:text-red-200',
+	},
+	gherkin: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	go: {
+		bg: 'bg-cyan-400/20',
+		border: 'border-cyan-400/50',
+		text: 'text-cyan-500 dark:text-cyan-300',
+	},
+	groovy: {
+		bg: 'bg-red-500/20',
+		border: 'border-red-500/50',
+		text: 'text-red-600 dark:text-red-400',
+	},
+	haskell: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	haxe: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	html: {
+		bg: 'bg-orange-500/20',
+		border: 'border-orange-500/50',
+		text: 'text-orange-600 dark:text-orange-400',
+	},
+	http: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	idl: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	java: {
+		bg: 'bg-red-500/20',
+		border: 'border-red-500/50',
+		text: 'text-red-600 dark:text-red-400',
+	},
+	javascript: {
+		bg: 'bg-yellow-500/20',
+		border: 'border-yellow-500/50',
+		text: 'text-yellow-600 dark:text-yellow-400',
+	},
+	jinja2: {
+		bg: 'bg-green-600/20',
+		border: 'border-green-600/50',
+		text: 'text-green-700 dark:text-green-300',
+	},
+	json: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	jsx: {
+		bg: 'bg-cyan-500/20',
+		border: 'border-cyan-500/50',
+		text: 'text-cyan-600 dark:text-cyan-400',
+	},
+	julia: {
+		bg: 'bg-indigo-500/20',
+		border: 'border-indigo-500/50',
+		text: 'text-indigo-600 dark:text-indigo-400',
+	},
+	kotlin: {
+		bg: 'bg-green-500/20',
+		border: 'border-green-500/50',
+		text: 'text-green-600 dark:text-green-400',
+	},
+	less: {
+		bg: 'bg-blue-600/20',
+		border: 'border-blue-600/50',
+		text: 'text-blue-700 dark:text-blue-300',
+	},
+	lezer: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	liquid: {
+		bg: 'bg-green-600/20',
+		border: 'border-green-600/50',
+		text: 'text-green-700 dark:text-green-300',
+	},
+	livescript: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	lua: {
+		bg: 'bg-gray-600/20',
+		border: 'border-gray-600/50',
+		text: 'text-gray-700 dark:text-gray-300',
+	},
+	markdown: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	mathematica: {
+		bg: 'bg-indigo-500/20',
+		border: 'border-indigo-500/50',
+		text: 'text-indigo-600 dark:text-indigo-400',
+	},
+	mbox: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	mermaid: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	mirc: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	modelica: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	mscgen: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	mumps: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	mysql: {
+		bg: 'bg-blue-800/20',
+		border: 'border-blue-800/50',
+		text: 'text-blue-900 dark:text-blue-100',
+	},
+	nesc: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	nginx: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	nix: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	nsis: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	ntriples: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	objectivec: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	objectivecpp: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	octave: {
+		bg: 'bg-indigo-500/20',
+		border: 'border-indigo-500/50',
+		text: 'text-indigo-600 dark:text-indigo-400',
+	},
+	oz: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	pascal: {
+		bg: 'bg-gray-600/20',
+		border: 'border-gray-600/50',
+		text: 'text-gray-700 dark:text-gray-300',
+	},
+	perl: {
+		bg: 'bg-gray-600/20',
+		border: 'border-gray-600/50',
+		text: 'text-gray-700 dark:text-gray-300',
+	},
+	pgsql: {
+		bg: 'bg-blue-800/20',
+		border: 'border-blue-800/50',
+		text: 'text-blue-900 dark:text-blue-100',
+	},
+	php: {
+		bg: 'bg-orange-500/20',
+		border: 'border-orange-500/50',
+		text: 'text-orange-600 dark:text-orange-400',
+	},
+	pig: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	powershell: {
+		bg: 'bg-gray-700/20',
+		border: 'border-gray-700/50',
+		text: 'text-gray-800 dark:text-gray-200',
+	},
+	properties: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	protobuf: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	puppet: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	python: {
+		bg: 'bg-blue-400/20',
+		border: 'border-blue-400/50',
+		text: 'text-blue-500 dark:text-blue-300',
+	},
+	q: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	r: {
+		bg: 'bg-indigo-500/20',
+		border: 'border-indigo-500/50',
+		text: 'text-indigo-600 dark:text-indigo-400',
+	},
+	ruby: {
+		bg: 'bg-red-600/20',
+		border: 'border-red-600/50',
+		text: 'text-red-700 dark:text-red-300',
+	},
+	rust: {
+		bg: 'bg-orange-600/20',
+		border: 'border-orange-600/50',
+		text: 'text-orange-700 dark:text-orange-300',
+	},
+	sas: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	sass: {
+		bg: 'bg-blue-600/20',
+		border: 'border-blue-600/50',
+		text: 'text-blue-700 dark:text-blue-300',
+	},
+	scala: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	scheme: {
+		bg: 'bg-purple-600/20',
+		border: 'border-purple-600/50',
+		text: 'text-purple-700 dark:text-purple-300',
+	},
+	shader: {
+		bg: 'bg-pink-500/20',
+		border: 'border-pink-500/50',
+		text: 'text-pink-600 dark:text-pink-400',
+	},
+	shell: {
+		bg: 'bg-gray-700/20',
+		border: 'border-gray-700/50',
+		text: 'text-gray-800 dark:text-gray-200',
+	},
+	sieve: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	smalltalk: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	solidity: {
+		bg: 'bg-pink-500/20',
+		border: 'border-pink-500/50',
+		text: 'text-pink-600 dark:text-pink-400',
+	},
+	solr: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	sparql: {
+		bg: 'bg-blue-800/20',
+		border: 'border-blue-800/50',
+		text: 'text-blue-900 dark:text-blue-100',
+	},
+	spreadsheet: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	sql: {
+		bg: 'bg-blue-800/20',
+		border: 'border-blue-800/50',
+		text: 'text-blue-900 dark:text-blue-100',
+	},
+	squirrel: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	stex: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	stylus: {
+		bg: 'bg-blue-600/20',
+		border: 'border-blue-600/50',
+		text: 'text-blue-700 dark:text-blue-300',
+	},
+	svelte: {
+		bg: 'bg-cyan-500/20',
+		border: 'border-cyan-500/50',
+		text: 'text-cyan-600 dark:text-cyan-400',
+	},
+	swift: {
+		bg: 'bg-green-500/20',
+		border: 'border-green-500/50',
+		text: 'text-green-600 dark:text-green-400',
+	},
+	tcl: {
+		bg: 'bg-gray-600/20',
+		border: 'border-gray-600/50',
+		text: 'text-gray-700 dark:text-gray-300',
+	},
+	textile: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	tiddlywiki: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	tiki: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	toml: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	troff: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	tsx: {
+		bg: 'bg-cyan-500/20',
+		border: 'border-cyan-500/50',
+		text: 'text-cyan-600 dark:text-cyan-400',
+	},
+	ttcn: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	turtle: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	typescript: {
+		bg: 'bg-blue-500/20',
+		border: 'border-blue-500/50',
+		text: 'text-blue-600 dark:text-blue-400',
+	},
+	vb: {
+		bg: 'bg-purple-500/20',
+		border: 'border-purple-500/50',
+		text: 'text-purple-600 dark:text-purple-400',
+	},
+	vbscript: {
+		bg: 'bg-purple-500/20',
+		border: 'border-purple-500/50',
+		text: 'text-purple-600 dark:text-purple-400',
+	},
+	velocity: {
+		bg: 'bg-green-600/20',
+		border: 'border-green-600/50',
+		text: 'text-green-700 dark:text-green-300',
+	},
+	verilog: {
+		bg: 'bg-red-700/20',
+		border: 'border-red-700/50',
+		text: 'text-red-800 dark:text-red-200',
+	},
+	vhdl: {
+		bg: 'bg-red-700/20',
+		border: 'border-red-700/50',
+		text: 'text-red-800 dark:text-red-200',
+	},
+	vue: {
+		bg: 'bg-cyan-500/20',
+		border: 'border-cyan-500/50',
+		text: 'text-cyan-600 dark:text-cyan-400',
+	},
+	wast: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	webidl: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	xquery: {
+		bg: 'bg-blue-800/20',
+		border: 'border-blue-800/50',
+		text: 'text-blue-900 dark:text-blue-100',
+	},
+	xml: {
+		bg: 'bg-green-600/20',
+		border: 'border-green-600/50',
+		text: 'text-green-700 dark:text-green-300',
+	},
+	yacas: {
+		bg: 'bg-gray-500/20',
+		border: 'border-gray-500/50',
+		text: 'text-gray-600 dark:text-gray-400',
+	},
+	yaml: {
+		bg: 'bg-yellow-400/20',
+		border: 'border-yellow-400/50',
+		text: 'text-yellow-500 dark:text-yellow-300',
+	},
+	z80: {
+		bg: 'bg-red-700/20',
+		border: 'border-red-700/50',
+		text: 'text-red-800 dark:text-red-200',
+	},
+};
+
+/**
+ * Get color configuration for a language
+ * Falls back to default colors if language is not found
+ */
+export function getLanguageColors(language: string): LanguageColorConfig {
+	const normalizedLanguage = language.toLowerCase().trim();
+
+	return (
+		languageColors[normalizedLanguage] || {
+			bg: 'bg-gray-500/20',
+			border: 'border-gray-500/50',
+			text: 'text-gray-600 dark:text-gray-400',
+		}
+	);
+}
+
+/**
+ * Get all available language colors (useful for previews or testing)
+ */
+export function getAllLanguageColors(): Record<string, LanguageColorConfig> {
+	return languageColors;
+}
+
+/**
+ * Get all supported languages
+ */
+export function getSupportedLanguages(): string[] {
+	return Object.keys(languageColors).sort();
+}
+
+/**
+ * Get the total count of supported languages
+ */
+export function getSupportedLanguagesCount(): number {
+	return Object.keys(languageColors).length;
+}
