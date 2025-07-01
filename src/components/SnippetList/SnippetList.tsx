a39a4555437bf2e10cffa2fed5fd76902355e1bb
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-07-01 07:09:30 +0100

    feat: enhance SnippetList to filter out deleting snippets during rendering

--- src/components/SnippetList/SnippetList.tsx
+++ src/components/SnippetList/SnippetList.tsx
@@ -1,13 +1,20 @@
+'use client';
+
 import Link from 'next/link';
 import SnippetCard from '@/components/SnippetCard/SnippetCard';
+import { useDeletingSnippets } from '@/contexts/DeletingSnippetsContext';
 
-export default async function SnippetList({ snippets }: { snippets: Snippet[] }) {
+export default function SnippetList({ snippets }: { snippets: Snippet[] }) {
+	const { isSnippetDeleting } = useDeletingSnippets();
+
 	return snippets.length > 0 ? (
-		snippets.map(({ id, title, language, content }) => (
-			<Link key={id} href={`/snippet/${id}`}>
-				<SnippetCard title={title} language={language} content={content} id={id} blurContent />
-			</Link>
-		))
+		snippets
+			.filter(({ id }) => !isSnippetDeleting(id)) // Filter out snippets that are being deleted
+			.map(({ id, title, language, content }) => (
+				<Link key={id} href={`/snippet/${id}`}>
+					<SnippetCard title={title} language={language} content={content} id={id} blurContent />
+				</Link>
+			))
 	) : (
 		<span className='italic'>No snippets to show</span>
 	);
