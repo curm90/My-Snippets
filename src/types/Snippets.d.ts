e8c3b52c3185fab43ad24e051933ed38255f41d6
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-07-01 17:41:34 +0100

    feat: extend Tag type definition to include createdAt and updatedAt fields

--- src/types/Snippets.d.ts
+++ src/types/Snippets.d.ts
@@ -1,7 +1,15 @@
+type Tag = {
+	id: string;
+	name: string;
+	createdAt: Date;
+	updatedAt: Date;
+};
+
 type Snippet = {
 	id: string;
 	title: string;
 	language: string;
 	content: string;
 	folderId?: string | null;
+	snippetTags?: Tag[];
 };
