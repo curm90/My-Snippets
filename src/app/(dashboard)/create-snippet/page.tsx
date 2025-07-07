0b0bb0a1d9ccc940c2eee058cee6a784ffc83609
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-07-07 12:05:25 +0100

    Move snippet crud ops and pages into hidden dashboard group route

--- src/app/(dashboard)/create-snippet/page.tsx
+++ src/app/(dashboard)/create-snippet/page.tsx
@@ -1,15 +1,18 @@
 import SnippetForm from '@/components/SnippetForm/SnippetForm';
 import { createSnippet } from '@/data-access/snippets';
-import prisma from '@/lib/prisma';
+import { getCurrentUser } from '@/lib/session';
+import { getFoldersForCurrentUser } from '@/data-access/folders';
 
 export default async function Page() {
-	const folders = await prisma.folder.findMany();
+	// This will redirect to /auth/signin if not authenticated
+	await getCurrentUser();
+	const folders = await getFoldersForCurrentUser();
 
 	return (
 		<div className='py-6 px-8'>
 			<h1 className='font-semibold text-xl'>Create a Snippet</h1>
 			<p className='text-sm text-muted-foreground mt-1'>
-				This information will be visible to others—please avoid sharing sensitive or private details.
+				This information will be visible to you—please avoid sharing sensitive or private details.
 			</p>
 			<div className='mt-8'>
 				<SnippetForm action={createSnippet} actionId='create' folders={folders} />
