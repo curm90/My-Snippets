e30e7503b14e3e1ab1e37467eeb29090752880c5
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-07-09 17:00:26 +0100

    refactor: reorganize Navigation component structure for improved readability

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,22 +3,38 @@ import Link from 'next/link';
 import ThemeToggle from '@/components/Theme/ThemeToggle';
 import { AuthButton } from '@/components/AuthButton/AuthButton';
 import SearchBarWrapper from '@/components/SearchBar/SearchBarWrapper';
+import MobileNavMenu from '@/components/MobileNavMenu/MobileNavMenu';
 
 export default function Navigation() {
 	return (
 		<nav className='fixed top-0 left-0 z-50 flex items-center justify-between py-4 px-8 bg-secondary w-full h-[var(--navbar-height)]'>
 			<div className='flex items-center space-x-6'>
+				{/* Logo */}
 				<Link href='/'>
 					<Image src='/logo.png' alt='code tags' width={30} height={30} className='rounded-full' />
 				</Link>
-				<Link href='/'>Dashboard</Link>
+
+				{/* Desktop Navigation */}
+				<Link href='/' className='hidden md:block'>
+					Dashboard
+				</Link>
 			</div>
-			<div className='flex items-center space-x-4 flex-1 justify-end px-6'>
-				<ThemeToggle />
+
+			{/* Search Bar & Theme */}
+			<div className='flex items-center space-x-4 flex-1 md:justify-end justify-center px-6'>
+				<div className='hidden md:block'>
+					<ThemeToggle />
+				</div>
 				<SearchBarWrapper />
 			</div>
-			<div className='flex items-center space-x-1'>
-				<AuthButton />
+
+			{/* Desktop Auth + Mobile Menu */}
+			<div className='flex items-center space-x-4'>
+				<div className='hidden md:block'>
+					<AuthButton />
+				</div>
+				{/* Mobile Navigation Menu */}
+				<MobileNavMenu />
 			</div>
 		</nav>
 	);
